df36f83bfe319d14e66264b03a6ca8f0c248acb8
Rafa <carlos09.cp65@gmail.com>
2023-06-28 16:18:29 -0600

    solcucion de La creación de la propiedad dinámica

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -19,6 +19,8 @@ use Psr\Log\LoggerInterface;
  *
  * For security be sure to declare any new methods as protected or private.
  */
+#[\AllowDynamicProperties]
+
 
 class BaseController extends Controller
 {
